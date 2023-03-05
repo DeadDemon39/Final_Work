ca423204aba68db7381d75d83ce1ae355dd52055
Aleksandr <deaddemon39@list.ru>
2023-03-05 18:48:36 +0300

    add new method

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -10,3 +10,13 @@
         }
     }
 }
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
